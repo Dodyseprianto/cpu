3abd800557c6203ae4e32dd28bd45a86e8027b9a
Dodyseprianto <59530482+Dodyseprianto@users.noreply.github.com>
2021-03-14 15:29:07 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
-
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://power2b.sea.mine.zpool.ca:6242 username: DSiwqzK1ZefaXhzwRJqtra9MKvHoN7DqDq password: c=DOGE
 
 done
